786d3c07810df6049d35038900ce31f66933e6e2
Globox21126 <aleksander.jaszczynski1996@gmail.com>
2020-09-20 19:05:51 +0200

    First day commit, app ready for coding

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import Products from "./Components/Products";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="test">
-      Hello World
-    </div>
+    <>
+    <Header />
+    <Products />
+    <Footer />
+    </>
   );
 }
 
